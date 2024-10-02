3841ace714b26f0e2792b8ade814702588533fd4
OliverO2 <oliver.o456i@gmail.com>
2024-10-02 18:31:07 +0200

    Fix root build script (harmonize plugin versions across subprojects)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,8 @@
 plugins {
-    alias(libs.plugins.org.jetbrains.kotlin.multiplatform) apply false
+    alias(libs.plugins.org.jetbrains.kotlin.multiplatform)
+    alias(libs.plugins.org.jetbrains.kotlin.jvm) apply false
+}
+
+kotlin {
+    jvm() // To keep the Gradle KMP plugin happy
 }
