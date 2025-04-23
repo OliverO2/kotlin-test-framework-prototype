52b0935c9ff938fe325ac6007433528e6e17dfc2
OliverO2 <oliver.o456i@gmail.com>
2025-04-23 22:31:55 +0200

    Build: Add prePublishingTests* tasks

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,3 +6,23 @@ plugins {
 kotlin {
     jvm() // To keep the Gradle KMP plugin happy
 }
+
+tasks {
+    for (type in listOf("allTests", "jvmTest")) {
+        register("prePublishingTests${type.capitalize()}") {
+            group = "verification"
+
+            dependsOn(":compiler-plugin:$type")
+            dependsOn(":gradle-plugin:test")
+            dependsOn(":framework-core:$type")
+
+            dependsOn(":integration-kotest-assertions:$type")
+            dependsOn(":integration-blocking-detection:$type")
+        }
+    }
+
+    register<Exec>("retryTestsUntilFailure") {
+        group = "verification"
+        commandLine = listOf("/bin/bash", "-c", "while gradlew -p framework-core cleanAllTests allTests; do true; done")
+    }
+}
